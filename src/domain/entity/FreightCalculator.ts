a4f97c59ac3b93b1c702cbcfac3f43ad7d41aef0
Suderland Weyner Gondim Guimaraes <suderland.guimaraes@serpro.gov.br>
2023-03-02 10:23:42 -0300

    última refatoração da aula

--- src/domain/entity/FreightCalculator.ts
+++ src/domain/entity/FreightCalculator.ts
@@ -3,7 +3,7 @@ import Product from "./Product";
 export default class FreightCalculator {
   static calculate (product: Product) {
     const volume = product.getVolume();
-    const density = product.weight/volume;
+    const density = product.getDensity();
     const itemFreight = 1000 * volume * (density/100);
     return itemFreight;
   }
--- src/domain/entity/Product.ts
+++ src/domain/entity/Product.ts
@@ -16,4 +16,8 @@ export default class Product {
   getVolume () {
     return this.width/100 * this.height/100 * this.length/100;
   }
+
+  getDensity () {
+    return this.weight/this.getVolume();
+  }
 }
