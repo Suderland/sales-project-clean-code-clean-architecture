acec8fe0be7c06fd8334ce7c881350804252895e
Suderland Weyner Gondim Guimaraes <suderland.guimaraes@serpro.gov.br>
2023-03-16 00:01:43 -0300

    calculando o frete

--- backend/freight/src/application/usecase/CalculateFreight.ts
+++ backend/freight/src/application/usecase/CalculateFreight.ts
@@ -1,17 +1,27 @@
+import DistanceCalculator from "../../domain/entity/DistanceCalculator";
 import FreightCalculator from "../../domain/entity/FreightCalculator";
+import ZipCodeRepository from "../repository/ZipCodeRepository";
 
 export default class CalculateFreight {
 
-	constructor () {
+	constructor (readonly zipCodeRepository: ZipCodeRepository) {
 	} 
 
 	async execute (input: Input): Promise<Output> {
 		const output: Output = {
 			freight: 0
 		};
+		let distance = 1000;
+		if (input.from && input.to) {
+			const from = await this.zipCodeRepository.get(input.from);
+			const to = await this.zipCodeRepository.get(input.to);
+			if (from && to) {
+				distance = DistanceCalculator.calculate(from.coord, to.coord);
+			}
+		}
 		if (input.items) {
 			for (const item of input.items) {
-				const itemFreight = FreightCalculator.calculate(item.width, item.height, item.length, item.weight, item.quantity);
+				const itemFreight = FreightCalculator.calculate(distance, item.width, item.height, item.length, item.weight, item.quantity);
 				output.freight += itemFreight;
 			}
 		}
@@ -21,6 +31,8 @@ export default class CalculateFreight {
 
 type Input = {
 	items: { width: number, height: number, length: number, weight: number, quantity: number }[],
+	from?: string,
+	to?: string
 }
 
 type Output = {
--- backend/freight/src/main_api.ts
+++ backend/freight/src/main_api.ts
@@ -1,4 +1,6 @@
+import ZipCodeRepository from "./application/repository/ZipCodeRepository";
 import CalculateFreight from "./application/usecase/CalculateFreight";
+import ZipCode from "./domain/entity/ZipCode";
 import PgPromise from "./infra/database/PgPromiseAdapter";
 import AxiosAdapter from "./infra/http/AxiosAdapter";
 import ExpressAdapter from "./infra/http/ExpressAdapter";
@@ -7,7 +9,17 @@ import HttpController from "./infra/http/HttpController";
 
 const connection  = new PgPromise();
 const httpClient = new AxiosAdapter();
-const calculateFreight = new CalculateFreight();
+const zipCodeRepository: ZipCodeRepository = {
+  async get (code: string): Promise<ZipCode | undefined> {
+    if (code === '22060030') {
+      return new ZipCode('22060030', '', '', -27.5945, -48.5477)
+    }
+    if (code === '88015600') {
+      return new ZipCode('88015600', '', '', -22.9129, -43.2003)
+    }
+  }
+}
+const calculateFreight = new CalculateFreight(zipCodeRepository);
 const httpServer = new ExpressAdapter();
 // const httpServer = new HapiHttpServer();
 
