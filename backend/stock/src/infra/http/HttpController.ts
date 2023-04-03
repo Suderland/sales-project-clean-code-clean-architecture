0b48566da5400b50a83ac274378be77717e54b7d
Suderland Weyner Gondim Guimaraes <suderland.guimaraes@serpro.gov.br>
2023-04-03 20:38:44 -0300

    antes de add fila

---
+++ backend/stock/src/infra/http/HttpController.ts
@@ -0,0 +1,24 @@
+import CalculateStock from "../../application/usecase/CalculateStock";
+import DecrementStock from "../../application/usecase/DecrementStock";
+import HttpServer from "./HttpServer";
+
+export default class HttpController {
+
+  constructor (
+    readonly httpServer: HttpServer, 
+    readonly decrementStock: DecrementStock,
+    readonly calculateStock: CalculateStock
+    ) {
+    
+    httpServer.on('post', '/decrementStock', async function (params:any, body:any) {
+      const output = decrementStock.execute(body);
+      return output;
+    });
+
+    httpServer.on('post', '/calculateStock', async function (params:any, body:any) {
+      const output = calculateStock.execute(body);
+      return output;
+    });
+
+  }
+}
